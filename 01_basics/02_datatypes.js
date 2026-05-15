762553ead4a1722d5b44b3bc49f06448f9372397
Tishalathwal <tishalathwal55@gmail.com>
2026-05-15 22:58:19 +0530

    Update console logs and variable values

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Tisha")
 
 
-let name = "hitesh"
+let name = "tisha"
 let age = 18
 let isLoggedIn = false
 let state;
