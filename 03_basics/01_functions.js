6b6ff4cad1a3003b03e7d32a3f04348082c1caec
Tishalathwal <tishalathwal55@gmail.com>
2026-05-20 17:53:52 +0000

    functions update

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,4 +1,4 @@
-// function 
+// *************function ***********************
 
 // function add(x, y) {
 //     console.log(x + y);
@@ -10,7 +10,7 @@ function add(x, y) {
     return x + y; // returning the sum of x and y
 }
 
-console.log(add(5, 10));
+// console.log(add(5, 10));
 
 function loggedinUser(username) {
     if (username === undefined) {
@@ -20,5 +20,39 @@ function loggedinUser(username) {
     }
 }
 
-loggedinUser("Tisha"); // calling the function with a username
-loggedinUser(); // calling the function without a username to trigger the undefined case
+// loggedinUser("Tisha");   // calling the function with a username
+// loggedinUser();   // calling the function without a username to trigger the undefined case
+
+
+// *************if wee want to take more parameters in the fxn but we are not sure how many parameters***********
+
+function sum(...numbers) { // using rest parameter
+    return numbers
+}
+
+// console.log(sum(1, 2, 3, 4, 5,6)); // calling the function with multiple arguments
+
+
+//  ******************calling a fxn with obj as an argument****************
+
+const user = {
+    name: "Tisha",
+    age: 22,
+    city: "Sonipat",
+}
+
+function displayUserInfo(anyObject) {
+    console.log(`Name: ${anyObject.name} and age: ${anyObject.age} and city: ${anyObject.city}`);
+}
+displayUserInfo(user); // calling the function with an object as an argument
+
+
+// ********calling a fxn with an array as an argument********
+
+const numbers = [1, 2, 3, 4, 5];
+
+function calculateSum(anyArr) {
+    return anyArr[3]
+}
+
+console.log(calculateSum(numbers)); // calling the function with an array as an argument
