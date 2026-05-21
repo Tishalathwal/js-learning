28d1f1c20716e6f855076347e8bd3fc38520814c
Tishalathwal <tishalathwal55@gmail.com>
2026-05-21 17:38:26 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,17 @@
+// ***********Switch Statement***********
+
+const weekDay = "Thursday";
+
+switch(weekDay){
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break;
+    default:
+        console.log("It's another day");
+}
