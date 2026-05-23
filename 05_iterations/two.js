22a7bceac32035c87627c06ca30293c223259497
Tishalathwal <tishalathwal55@gmail.com>
2026-05-23 09:33:09 +0000

    While Do-While

---
+++ 05_iterations/two.js
@@ -0,0 +1,37 @@
+// *************while loop**************88
+
+/*
+let i =1
+while (i<=5) {
+    console.log(i);
+    i++
+}
+*/
+
+
+/*
+let myarr = ['apple', 'mango', 'banana'];
+let arr = 0;
+while (arr < myarr.length) {
+    console.log(myarr[arr]);
+    arr++;
+}
+*/
+
+
+
+// ************do while loop***************
+
+/*
+let i = 0;
+do{
+    console.log(i);
+    i++;
+} while (i<10)
+*/
+
+let j = 11
+do{                   // this loop will execute at least once even if the condition is false
+    console.log(j);
+    j++;
+} while (j<=10)
